f560c8410128b308f4be006d79f7e405dd3f3810
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-08-09 03:38:10 -0300

    Corrige posicao do array no JavaVisitor

--- Exercicios/exercicio07-GeracaoCodigoEmC++/source2source/compileOnlyBACKUP.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-cls
-javac -cp .;beaver-rt-0.9.11.jar;ST-4.3.1.jar;antlr-3.5.2-runtime.jar -d . ast/*.java
-javac -cp .;beaver-rt-0.9.11.jar;ST-4.3.1.jar;antlr-3.5.2-runtime.jar -d . langUtil/*.java
-javac -cp .;beaver-rt-0.9.11.jar;ST-4.3.1.jar;antlr-3.5.2-runtime.jar -d . parsers/*.java
-javac -cp .;beaver-rt-0.9.11.jar;ST-4.3.1.jar;antlr-3.5.2-runtime.jar -d . visitors/*.java
-javac -cp .;beaver-rt-0.9.11.jar;ST-4.3.1.jar;antlr-3.5.2-runtime.jar -d . MiniLang.java
-pause
