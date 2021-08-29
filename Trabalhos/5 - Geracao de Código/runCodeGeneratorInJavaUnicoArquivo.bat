c0d737be5da9bd82307440a58507ddd5a74b9078
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-08-29 05:35:49 -0300

    Mudança no Iterate:
    => Como o iterate é um comando de repetição que repete um número de vezes, logo foi necessario colocar outra variavel para pegar o final do loop, pq caso usasse a variavel de expressao e ela sofresse alteração, daria problema no número de repetições

--- Trabalhos/5 - Geracao de Código/runCodeGeneratorInJavaUnicoArquivo.bat
+++ Trabalhos/5 - Geracao de Código/runCodeGeneratorInJavaUnicoArquivo.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 cls
 echo "----  EXECUTANDO O CODE-GENERATOR ----"
-java -cp lib/ANTLR.jar;lib/ST-4.3.1.jar;. lang/LangCompiler -Java ./testes/semantica/certo/teste12.lan
+java -cp lib/ANTLR.jar;lib/ST-4.3.1.jar;. lang/LangCompiler -Java ./testes/semantica/certo/teste11.lan
 
 echo "----  EXECUCAO CONCLUIDA COM SUCESSO ----"
 pause
